8e9bf8d7031963b98bc7a637e34a7de9efe4fc66
Owen Johnson <owenbjohnson10@gmail.com>
2023-02-21 09:44:36 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Owen Johnson on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
